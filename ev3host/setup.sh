6d964ccbea380a5b9dc82a15defb2707650ddd1c
Joon Yee Chuah <jchuah@tacc.utexas.edu>
2016-03-02 15:38:19 -0600

    setup script

--- ev3host/setup.sh
+++ ev3host/setup.sh
@@ -2,15 +2,13 @@
 set -e
 echo "Launching ev3host setup"
 
-apt-get update
-apt-get install python python-dev ev3ev
 easy_install pika
-easy_install rpyc_classic
+easy_install rpyc
 chmod +x ev3host
 chmod +x ev3host.py
 cp ev3host /etc/init.d/
 cp ev3host.py /usr/local/bin
-update-rc.d /etc/init.d/ev3host defaults
+update-rc.d ev3host defaults
 /etc/init.d/ev3host start
 
 echo "ev3host should be up and running now!"
